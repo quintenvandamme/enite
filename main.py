371783af67245022c0b273042a2c9a6db86b7556
Quinten <quintenvandamme555@gmail.com>
2021-03-29 09:03:23 +0000

    0.1.5

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 # import libs
 import discord
 import os
+import random
 from keep_alive import keep_alive
 from discord.ext import commands
 import datetime
 
 # version
-VERSION = "0.1.4"
+VERSION = "0.1.5"
 GITHUB = "https://github.com/HexaOneOfficial/enite"
-BUMP = "!d bump"
 
+# intents
+intents = discord.Intents.default()
+intents.members = True
 
-client = discord.Client()
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
@@ -30,6 +33,12 @@ async def on_message(message):
 
   if message.content.startswith('/github'):
     await message.channel.send(GITHUB)
+
+@client.event
+async def on_member_join(member):
+  guild = client.get_guild(753325735137116362)
+  channel = guild.get_channel(753325735795490929)
+  await channel.send(f'Welcome! {member.mention}')
 
 keep_alive()
 
