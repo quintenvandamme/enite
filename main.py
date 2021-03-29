73a119dd72d35c48c9b24686d6375d7af0e807a8
Quinten <quintenvandamme555@gmail.com>
2021-03-29 07:30:18 +0000

    0.1.4

--- main.py
+++ main.py
@@ -2,16 +2,23 @@
 import discord
 import os
 from keep_alive import keep_alive
+from discord.ext import commands
+import datetime
 
 # version
-VERSION = "0.1.3"
+VERSION = "0.1.4"
 GITHUB = "https://github.com/HexaOneOfficial/enite"
+BUMP = "!d bump"
+
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
+  await client.change_presence(status=discord.Status.idle, activity=activity)
   print('Welcome Hexa')
+activity = discord.Game(name="Being a cat")
+  
 
 @client.event  
 async def on_message(message):
