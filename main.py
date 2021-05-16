0224118d0d214dfef5e3cea54514f04b994a5988
Quinten <58103738+hexa-one@users.noreply.github.com>
2021-05-16 21:30:48 +0200

    0.1.9

--- main.py
+++ main.py
@@ -2,18 +2,16 @@ import discord
 import sys
 import random
 import time
+import datetime
+import os
 from keep_alive import keep_alive
 from discord.ext import commands
 sys.path.append('/mnt/disk1/bot/')
 from TOKEN_ENITE import token
-import datetime
 
-# version
-VERSION = "0.1.8"
+# bot info
+VERSION = "0.1.9"
 SOURCE = "https://github.com/hexa-one/enite"
-
-# commands_id
-MEOW = "meow :cat2: :cat2:"
 
 # intents
 intents = discord.Intents.default()
@@ -37,8 +35,8 @@ async def on_message(message):
   # server id
   guild_id = client.get_guild(753325735137116362)
 
-  # mod users
-  mod_users = ["hexa#0069","Lars#8503","Twg#6969"]
+  # mods
+  mod_users = ["hexa#9096","Lars#8503","furkmeenistan#3286"]
   
   # commands
   if message.author == client.user:
@@ -51,33 +49,33 @@ async def on_message(message):
     await message.channel.send(SOURCE)
 
   if message.content.startswith('/meow'):
+    MEOW = "meow :cat2: :cat2:"
     await message.channel.send(MEOW)
 
-  ## test only
-  if message.content.startswith('/build-ionix'):
-    guild = client.get_guild(753325735137116362)
-    channel = guild.get_channel(826028712474574888)
-    file = discord.File("assets/cat.png")
-    await channel.send("hexa's pfp", file=file)
+  if message.content.startswith('/date'):
+    today = datetime.date.today()
+    DATE = today.strftime("20%y/%m/%d")
+    await message.channel.send(DATE)
 
-  if message.content.startswith('/build-album'):
-    guild = client.get_guild(753325735137116362)
-    channel = guild.get_channel(825694784567115776)
-    await channel.send(f"""```Starlight2022-rc1``` `Download:` https://gitlab.com/HexaOneOfficial/starlight-2022/-/archive/master/starlight-2022-master.zip""")
-
+  ## latency (beta)
   if message.content.startswith('/latency'):
     time_1 = time.perf_counter()
     time_2 = time.perf_counter()
     ping = round((time_2-time_1)*1000)
     await message.channel.send(f"ping = {ping}")
 
-## old stats code
-  #if message.content == "/stats":
-    #await message.channel.send(f"""```Members: {guild_id.member_count}
-#Version: {VERSION}
-#Source: {SOURCE}```""")
 
-  ## stats command (embed)
+## help command (embed) (beta)
+  if message.content.startswith('/help'):
+    embedVar = discord.Embed(title="Stats", description="", color="#3451E6")
+    embedVar.add_field(name="Latency:", value=("/latency"), inline=True)
+    embedVar.add_field(name="Bot Version:", value=("/version"), inline=False)
+    embedVar.add_field(name="Test Mod:", value=("/testmod"), inline=False)
+    embedVar.add_field(name="Date:", value=("/date"), inline=False)
+    await message.channel.send(embed=embedVar)
+
+
+## stats command (embed)
   if message.content.startswith('/stats'):
     embedVar = discord.Embed(title="Stats", description="", color=0x00ff00)
     embedVar.add_field(name="Members:", value=(guild_id.member_count), inline=True)
@@ -100,6 +98,8 @@ async def on_message(message):
     await channel.send(f"""```User:{message.author} tried {message.content} in #{message.channel}```""")
   if message.content.startswith('!'):
     await channel.send(f"""```User:{message.author} tried {message.content} in #{message.channel}```""")
+  if message.content.startswith('?'):
+    await channel.send(f"""```User:{message.author} tried {message.content} in #{message.channel}```""")
 
 # welcome users
 @client.event
@@ -108,7 +108,7 @@ async def on_member_join(member):
   channel = guild.get_channel(753325735795490929)
   await channel.send(f'Welcome! {member.mention}')
 
-keep_alive()
+keep_alive() # for backup only
 
 client.run(token)
 
